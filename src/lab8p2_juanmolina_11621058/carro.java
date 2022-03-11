5f6d89bd9576ba8229ef6aeae23672f84ded3d7e
hwanma <jmmolv@gmail.com>
2022-03-11 14:49:23 -0600

    second commit
    objeto carros

---
+++ src/lab8p2_juanmolina_11621058/carro.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8p2_juanmolina_11621058;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Hwan
+ */
+public class carro {
+    private int id,distancia,velocidad;
+    private String nombre, tipo;
+    private Color color;
+
+    public carro() {
+    }
+
+    public carro(int id, String nombre, String tipo, Color color) {
+        this.id = id;
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    
+    
+}
