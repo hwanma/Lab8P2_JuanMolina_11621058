8101588c519719c96aab6bde26b1c0998a27d073
hwanma <jmmolv@gmail.com>
2022-03-11 15:22:27 -0600

    third commit
    archivo metodos

---
+++ src/lab8p2_juanmolina_11621058/archivo.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8p2_juanmolina_11621058;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Hwan
+ */
+public class archivo {
+    private ArrayList<carro> carros = new ArrayList();
+    private File archivo = null;
+    
+    public archivo(String path) {
+        archivo = new File(path);
+    }
+
+    public ArrayList<carro> getListaPersonas() {
+        return carros;
+    }
+
+    public void setListaPersonas(ArrayList<carro> listaAlumnos) {
+        this.carros = listaAlumnos;
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+    
+    public void setAlumno(carro a){
+        carros.add(a);
+    }
+    
+    public void cargarArchivo() {
+        try {            
+            carros = new ArrayList();
+            carro temp;
+            if (archivo.exists()) {
+                FileInputStream entrada
+                    = new FileInputStream(archivo);
+                ObjectInputStream objeto
+                    = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (carro) objeto.readObject()) != null) {
+                        carros.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del archivo
+                }
+                objeto.close();
+                entrada.close();
+            }            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+        try {
+            fw = new FileOutputStream(archivo);
+            bw = new ObjectOutputStream(fw);
+            for (carro t : carros) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
+}
